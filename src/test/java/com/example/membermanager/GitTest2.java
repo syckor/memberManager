9e2aac14d83d20a1c5bb6815890d068be67e6d8d
syckor <syckor@naver.com>
2023-07-02 21:52:00 +0900

    tester

---
+++ src/test/java/com/example/membermanager/GitTest2.java
@@ -0,0 +1,13 @@
+package com.example.membermanager;
+
+import lombok.extern.slf4j.Slf4j;
+import org.junit.jupiter.api.Test;
+
+@Slf4j
+public class GitTest2 {
+
+    @Test
+    public void test(){
+        log.info("tester");
+    }
+}
